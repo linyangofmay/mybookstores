8fbc6c643b11a229d32e3303cf8456c75db66bae
linyangofmay <linyangofmay@gmail.com>
2022-10-27 13:02:54 -0700

    resetherokudata

---
+++ react-app/src/store/bookstore.js
@@ -0,0 +1,8 @@
+const createBookstore = '/bookstore/createBookstore'
+const getAllBookstore = '/bookstore/getAllProduct'
+const getCurrentBookstore = '/bookstore/getCurrentBookstore'
+const getOneBookstore = '/bookstore/getOneBookstore'
+const updateBookstore = '/bookstore/updateBookstore'
+const deleteBookstore = '/bookstore/deleteBookstore'
+
+
